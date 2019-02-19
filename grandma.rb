1fd44e631200a1a2ca4cee047806939ba84fd898
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-02-19 20:13:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@ elsif phrase == phrase.downcase || phrase.capitalize
   return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
-elsif (phrase == phrase.upcase() && (phrase != "I LOVE YOU GRANDMA!")
+elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
   return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
 else
