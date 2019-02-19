84784f2a6d6fe04ec29a81743ae232bf4fd89ae6
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-02-19 19:56:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,8 @@ elsif phrase == phrase.downcase || phrase.captialize
   return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
+elsif phrase == phrase.upcase
+  return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
 else
   return "NO, NOT SINCE 1938!"
