5c5819cf20ed12b952714f98b0664f3c6c0c9c7c
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-02-19 19:45:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 def speak_to_grandma(phrase)
  
 if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase.upcase && (speak_to_grandma != "I LOVE YOU GRANDMA!")
-  puts "NO, NOT SINCE 1938!"
-else
+  return "I LOVE YOU TOO PUMPKIN!"
+elsif phrase.upcase
+  return "NO, NOT SINCE 1938!"
+elsif phrase.downcase
   puts "HUH?! SPEAK UP SONNY!"
   
 end
