b95e897f8aabc90aa833d0d5f4124fcde78899d3
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-02-19 19:55:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,9 +7,9 @@ if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-elsif phrase == phrase.downcase
+elsif phrase == phrase.downcase || phrase.capitalize
   return "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.downcase
+elsif phrase == phrase.downcase || phrase.captialize
   return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
